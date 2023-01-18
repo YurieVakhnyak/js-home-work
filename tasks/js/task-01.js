3a3d7bab2c9b40253477b491fc91b3770b789436
YVakhnyak <namejfk@gmail.com>
2023-01-19 01:12:09 +0200

    task-01 add

---
+++ tasks/js/task-01.js
@@ -0,0 +1,11 @@
+const categories = document.querySelector("ul#categories");
+console.log(`Number of categories: ${categories.children.length}`);
+const categoriesItems = categories.querySelectorAll("li.item");
+
+categoriesItems.forEach(function (category) {
+  const titleText = category.querySelector("h2");
+  console.log(`Category: ${titleText.textContent}`);
+
+  const elements = category.querySelectorAll("ul li");
+  console.log(`Elements: ${elements.length}`);
+});
