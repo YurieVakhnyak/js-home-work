aaaa70e0bbf29aade551971cad39575b5daa791f
YVakhnyak <namejfk@gmail.com>
2023-01-24 16:48:59 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,17 +2,11 @@ const textInput = document.querySelector("#validation-input");
 textInput.value
 textInput.addEventListener("blur", () => {
     let length = textInput.value.length;    
-     
-    if (length !== 6)   
-    textInput.classList.add("invalid");  
-      else 
-    textInput.classList.remove("invalid");
-    textInput.classList.add("valid");
-   
-        
-
+    const dataLength = parseInt(textInput.getAttribute('data-length'))
+    if (length !== dataLength)   
+        textInput.classList.add("invalid"); 
+    else 
+        textInput.classList.remove("invalid");
+        textInput.classList.add("valid");
 });
 
-// textInput.addEventListener("focus", () => {
-//     textInput.value = "";
-// });
