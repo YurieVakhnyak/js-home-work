a6699f0f192c307de28880136a4ccce7b0f3605d
YVakhnyak <namejfk@gmail.com>
2023-01-25 00:31:01 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const color = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", (event) => {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  let randomHexColor = getRandomHexColor();
+  color.textContent = randomHexColor;
+  const body = document.querySelector("body");
+  body.setAttribute("style", `background-color:${randomHexColor}`);
+})
